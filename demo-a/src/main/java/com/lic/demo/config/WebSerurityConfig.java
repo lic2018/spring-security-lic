e017e510363e3dea56784115679685ff85a8faf8
lic <1099959038@qq.com>
2020-06-04 17:18:06 +0800

    Initial commit

---
+++ demo-a/src/main/java/com/lic/demo/config/WebSerurityConfig.java
@@ -0,0 +1,12 @@
+package com.lic.demo.config;
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+/**
+ * @Author: lic
+ * @date: 2020/6/4
+ */
+@EnableWebSecurity
+public class WebSerurityConfig extends WebSecurityConfigurerAdapter {
+}
